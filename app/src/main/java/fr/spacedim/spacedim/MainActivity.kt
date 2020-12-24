dab5ab960d6af671c2cc38216b3361f2f3545299
Nathan Cuvellier <cuvellier.nathan@gmail.com>
2020-12-24 15:36:25 +0100

    log Cyclelife for login page

--- app/src/main/java/fr/spacedim/spacedim/MainActivity.kt
+++ app/src/main/java/fr/spacedim/spacedim/MainActivity.kt
@@ -2,10 +2,42 @@ package fr.spacedim.spacedim
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
+
+    private val tag = "Login page"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.i(tag,"onCreate")
     }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i(tag,"onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i(tag,"onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i(tag,"onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i(tag,"onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i(tag,"onDestroy")
+    }
+
 }
