b02abc3b17221970dfdb407fa9df5c76b78bb96c
Nathan Cuvellier <cuvellier.nathan@gmail.com>
2021-01-02 11:01:09 +0100

    change file name, add title on login page and background test

--- app/src/main/java/fr/test200/spacedim/LoginActivity.kt
+++ app/src/main/java/fr/test200/spacedim/LoginActivity.kt
@@ -4,13 +4,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
 
     private val tag = "Login page"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
 
         Log.i(tag,"onCreate")
     }
