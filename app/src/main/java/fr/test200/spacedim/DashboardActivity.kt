2c7022942d33167a14d8213e09e7afa24874c6f9
Loic Viard Crétat <loicviard12@gmail.com>
2021-01-05 09:14:26 +0100

    [uncorrected error] links between pages (waiting + dashboard)

--- app/src/main/java/fr/test200/spacedim/DashboardActivity.kt
+++ app/src/main/java/fr/test200/spacedim/DashboardActivity.kt
@@ -1,50 +1,50 @@
 package fr.test200.spacedim
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
-class WaitingRoomActivity : AppCompatActivity() {
-
-    private val tag = "Waiting activity"
+class DashboardActivity : AppCompatActivity() {
+    private val tag = "Dashboard page"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_waiting_room)
+        setContentView(R.layout.activity_end_layout)
 
-        val btnJoin = findViewById<Button>(R.id.buttonJoinRoom)
-        btnJoin.setOnClickListener {
-            val intent = Intent(this, DashboardActivity::class.java)
+        val btnWin = findViewById<Button>(R.id.winButton)
+        btnWin.setOnClickListener {
+            val intent = Intent(this, EndActivity::class.java)
             startActivity(intent)
         }
 
         Log.i(tag, "onCreate")
     }
+
     override fun onStart() {
         super.onStart()
-        Log.i(tag,"onStart")
+        Log.i(tag, "onStart")
     }
 
     override fun onResume() {
         super.onResume()
-        Log.i(tag,"onResume")
+        Log.i(tag, "onResume")
     }
 
     override fun onPause() {
         super.onPause()
-        Log.i(tag,"onPause")
+        Log.i(tag, "onPause")
     }
 
     override fun onStop() {
         super.onStop()
-        Log.i(tag,"onStop")
+        Log.i(tag, "onStop")
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        Log.i(tag,"onDestroy")
+        Log.i(tag, "onDestroy")
     }
 
     override fun onRestart() {
