7cf9926f91056bacee97a5eb106886976d84ab62
Loic Viard Crétat <loicviard12@gmail.com>
2021-01-12 14:05:08 +0100

    add score (livedata)

--- app/src/main/java/fr/test200/spacedim/dashboard/DashboardFragment.kt
+++ app/src/main/java/fr/test200/spacedim/dashboard/DashboardFragment.kt
@@ -5,24 +5,41 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.findNavController
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import fr.test200.spacedim.MainActivity
 import fr.test200.spacedim.R
 import fr.test200.spacedim.databinding.DashboardFragmentBinding
 
 class DashboardFragment : Fragment() {
 
+    private lateinit var binding: DashboardFragmentBinding
+
     private lateinit var viewModel: DashboardViewModel
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View {
-        val binding: DashboardFragmentBinding = DataBindingUtil.inflate(
-            inflater, R.layout.dashboard_fragment, container, false)
 
-        binding.winButton.setOnClickListener {
-            findNavController().navigate(DashboardFragmentDirections.actionDashboardFragmentToEndFragment())
-        }
+        binding = DataBindingUtil.inflate(
+                inflater,
+                R.layout.dashboard_fragment,
+                container,
+                false
+        )
+
+        viewModel = ViewModelProvider(this).get(DashboardViewModel::class.java)
+
+        binding.dashboardViewModel = viewModel
+        binding.lifecycleOwner = viewLifecycleOwner
+
+        viewModel.moduleNumber.observe(viewLifecycleOwner, Observer { newModuleNumber ->
+            for (indexModule in 1..newModuleNumber) {
+                val button = Button(this.context)
+            }
+        })
 
         return binding.root
     }
