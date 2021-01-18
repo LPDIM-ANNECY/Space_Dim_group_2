77cc66e1df04c5debdb60deceadbdefb792a842a
Nathan Cuvellier <cuvellier.nathan@gmail.com>
2021-01-18 12:49:52 +0100

    add sound

--- app/src/main/java/fr/test200/spacedim/MainActivity.kt
+++ app/src/main/java/fr/test200/spacedim/MainActivity.kt
@@ -2,11 +2,18 @@ package fr.test200.spacedim
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI.navigateUp
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController : NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
+        navController = findNavController(R.id.nav_host_fragment)
     }
+
 }
